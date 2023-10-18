d5177c41e8665c0b242a772457dfb34b78f7ac52
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-10-19 00:58:44 +0600

    create put api to update the product

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k4mfqh2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,12 +30,43 @@ async function run() {
       const result = await productCollections.insertOne(product);
       res.send(result);
     });
+    // Get Product Brand Wise
     app.get("/product/:brand", async (req, res) => {
       const brand = req.params.brand;
       const query = { selectedBrand: brand };
       const result = await productCollections.find(query).toArray();
       res.send(result);
     });
+    // Get Single Product
+    app.get("/product-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollections.findOne(query);
+      res.send(result);
+    });
+
+    // update a signle product
+    app.put("/product-update/:_id", async (req, res) => {
+      const id = req.params._id;
+      const updatedProductData = req.body;
+      console.log(updatedProductData)
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateProduct = {
+        $set: {
+          product_img: updatedProductData.product_img,
+          product_name: updatedProductData.product_name,
+          product_price: updatedProductData.product_price,
+          product_description: updatedProductData.product_description,
+          selectedBrand: updatedProductData.updatedBrand,
+          selectedCategory: updatedProductData.updatedCategory,
+          rating: updatedProductData.updatedRating,
+        },
+      };
+      const result = await productCollections.updateOne(filter, updateProduct, options);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
